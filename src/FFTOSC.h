447f53d37a3217abe028e4919a3016c8f1d91320
John Thompson <jthompson@georgiasouthern.edu>
2026-05-04 15:47:49 -0400

    Replace JuceHeader.h with explicit module includes; add juce_audio_formats

--- src/FFTOSC.h
+++ src/FFTOSC.h
@@ -1,6 +1,12 @@
 #pragma once
 
-#include <JuceHeader.h>
+#include <juce_core/juce_core.h>
+#include <juce_events/juce_events.h>
+#include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_audio_formats/juce_audio_formats.h>
+#include <juce_audio_devices/juce_audio_devices.h>
+#include <juce_dsp/juce_dsp.h>
+#include <juce_osc/juce_osc.h>
 #include <mutex>
 #include <atomic>
 
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,4 +1,3 @@
-#include <JuceHeader.h>
 #include "FFTOSC.h"
 #include <thread>
 #include <chrono>
